2a4f0c7cadb771524a618c7ae8ffb248feb76594
swaraliGujrathi <43879396+swaraliGujrathi@users.noreply.github.com>
2020-08-26 08:47:57 +0530

    Create Shape.java

---
+++ Assignment2/Shape.java
@@ -0,0 +1,21 @@
+public class Shape {
+   private String name;
+   
+   public Shape(){
+	   this.name = null;
+   }
+   
+   public Shape(String name){
+	   this.name = name;
+   }
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+   
+   
+}
